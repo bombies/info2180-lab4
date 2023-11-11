1cdeaf0ee39930dadd991db90fe9bad2177fc286
bombies <xjamesdzn@gmail.com>
2023-11-11 18:29:55 -0500

    Implement front-end integration and styling

--- app.js
+++ app.js
@@ -1,17 +1,60 @@
+// (() => {
+//     const searchBtn = document.getElementById("search")
+//     if (!searchBtn)
+//         return;
+
+//     searchBtn.addEventListener('click', async () => {
+//         const res = await fetch("superheroes.php", {
+//             method: "GET"
+//         })
+
+//         if (!res.ok)
+//             return console.error(res)
+
+//         const data = await res.text()
+//         alert(data)
+//     })
+// })()
+
+// Handle form submit
 (() => {
-    const searchBtn = document.getElementById("search")
-    if (!searchBtn)
+    const form = document.getElementById("superhero_form")
+    if (!form)
         return;
 
-    searchBtn.addEventListener('click', async () => {
-        const res = await fetch("superheroes.php", {
-            method: "GET"
-        })
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault()
 
-        if (!res.ok)
-            return console.error(res)
+        const data = new FormData(form)
+        const DATA_REGEX = /[a-zA-Z\s]+/g
 
-        const data = await res.text()
-        alert(data)
+        // Filter unwanted characters
+        const name = data.get("name") ? data.get("name").match(DATA_REGEX).join('') : ""
+        const res = await fetch(`superheroes.php?query=${name}`)
+
+        const resultDiv = document.getElementById("result")
+        if (!resultDiv)
+            return;
+
+        if (!res.ok) {
+            resultDiv.innerHTML = "<p class=\"error-msg\">Superhero not found</p>"
+            return;
+        }
+
+        const fetchedData = await res.json()
+        if (!fetchedData.length) {
+            resultDiv.innerHTML = "<p class=\"error-msg\">Superhero not found</p>"
+            return;
+        }
+
+        resultDiv.innerHTML = name !== "" ? fetchedData.map(entry => `
+        <h2>${entry.name}</h2>
+        <h3>A.K.A ${entry.alias}</h3>
+        <article><p>${entry.biography}</p></article>
+        `).join("") : (`
+        <ul>
+        ${fetchedData.map(entry => `<li>${entry.alias}</li>`).join("")}
+        </ul>
+        `)
     })
 })()
