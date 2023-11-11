aae94db9bd918aa6d6686874d22997fdd7f4f952
bombies <xjamesdzn@gmail.com>
2023-11-11 18:32:34 -0500

    Remove old code

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// (() => {
-//     const searchBtn = document.getElementById("search")
-//     if (!searchBtn)
-//         return;
-
-//     searchBtn.addEventListener('click', async () => {
-//         const res = await fetch("superheroes.php", {
-//             method: "GET"
-//         })
-
-//         if (!res.ok)
-//             return console.error(res)
-
-//         const data = await res.text()
-//         alert(data)
-//     })
-// })()
-
 // Handle form submit
 (() => {
     const form = document.getElementById("superhero_form")
